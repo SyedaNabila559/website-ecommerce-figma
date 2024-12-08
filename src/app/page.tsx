4032982f2b0f92d9c48c1c984824088f1943fe27
SyedaNabila559 <abidkhan78000@gmail.com>
2024-12-08 01:04:25 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,183 @@
-import Image from "next/image";
+import Category from '@/components/Category'
+import CategoryCard from '@/components/CategoryCard'
+import ExploreProducts from '@/components/ExploreProducts'
+import Features from '@/components/Features'
+import HeroSection from '@/components/HeroSection'
+import MoreProducts from '@/components/MoreProduct'
+import MusicSection from '@/components/MusicSection'
+import Product from '@/components/Product'
+import ProductCard from '@/components/ProductCard'
+import SalesCard from '@/components/SalesCard'
+import Today from '@/components/Today'
+import React from 'react'
+import Explore from '@/components/Explore'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const page = () => {
+    return (
+        <div>
+            <HeroSection />
+            <Today />
+            <div className="flex flex-wrap gap-4 justify-center lg:justify-evenly mt-8">
+                <SalesCard
+                    image="/images/gamepad.png"
+                    heading="HAVIT HV-G92 Gamepad"
+                    price="$120"
+                    lowPrice="$160"
+                    rating="/images/five-star.png"
+                    category="(88)"
+                />
+                <SalesCard
+                    image="/images/keyboard.png"
+                    heading="AK-900 Wired Keyboard"
+                    price="$960"
+                    lowPrice="$1160"
+                    rating="/images/four-star.png"
+                    category="(75)"
+                />
+                <SalesCard
+                    image="/images/moniter.png"
+                    heading="IPS LCD Gaming Monitor"
+                    price="$370"
+                    lowPrice="$400"
+                    rating="/images/five-star.png"
+                    category="(99)"
+                />
+                <SalesCard
+                    image="/images/chair.png"
+                    heading="S-Series Comfort Chair"
+                    price="$370"
+                    lowPrice="$400"
+                    rating="/images/half-star.png"
+                    category="(99)"
+                />
+            </div>
+            <Link href={"/product-detailed-page"}>
+                <div className="flex items-center justify-center mt-8 sm:mt-10 lg:mt-14">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                    <button className="bg-[#DB4444] text-white text-[14px] sm:text-[16px] w-[80%] max-w-[234px] h-[48px] sm:h-[56px] rounded-md hover:bg-[#C73333] transition-all">
+                        View All Products
+                    </button>
+
+                </div>
+            </Link>
+            <Category />
+            <div className="flex flex-wrap justify-center lg:justify-between gap-6 mt-6 px-4 lg:px-0">
+                <CategoryCard image="/images/mobile.png" heading="Phones" />
+                <CategoryCard image="/images/computer.png" heading="Computers" />
+                <CategoryCard image="/images/smartwatch.png" heading="SmartWatch" />
+                <CategoryCard image="/images/camera.svg" heading="Camera" />
+                <CategoryCard image="/images/headphone.png" heading="HeadPhones" />
+                <CategoryCard image="/images/category-gamepad.png" heading="Gaming" />
+            </div>
+            <Product />
+            <div className="flex flex-wrap justify-center gap-6 mt-6 px-4 lg:px-0">
+                <ProductCard
+                    image='/images/shirt.png'
+                    heading='The north coat'
+                    price='$260'
+                    lowPrice='$360'
+                    rating="/images/five-star.png"
+                    category='(65)'
+                />
+                <ProductCard
+                    image='/images/bag.png'
+                    heading='Gucci duffle bag'
+                    price='$960'
+                    lowPrice='$1160'
+                    rating="/images/half-star.png"
+                    category='(65)'
+                />
+                <ProductCard
+                    image='/images/cpu.png'
+                    heading='RGB liquid CPU Cooler'
+                    price='$160'
+                    lowPrice='$170'
+                    rating="/images/half-star.png"
+                    category='(65)'
+                />
+                <ProductCard
+                    image='/images/books.png'
+                    heading='Small BookSelf'
+                    price='$360'
+                    lowPrice='$400'
+                    rating="/images/five-star.png"
+                    category='(65)'
+                />
+            </div>
+
+            <MusicSection />
+            <Explore />
+
+            <div className='flex flex-wrap justify-center gap-6 mt-6'>
+                <ExploreProducts
+                    image='/images/dog.png'
+                    heading='Breed Dry Dog Food'
+                    price='$100'
+                    rating="/images/three-star.png"
+                    category='(35)'
+                />
+                <ExploreProducts
+                    image='/images/new-camera.png'
+                    heading='CANON EOS DSLR Camera'
+                    price='$360'
+                    rating="/images/four-star.png"
+                    category='(95)'
+                />
+                <ExploreProducts
+                    image='/images/laptop.png'
+                    heading='ASUS FHD Gaming Laptop'
+                    price='$700'
+                    rating="/images/five-star.png"
+                    category='(325)'
+                />
+                <ExploreProducts
+                    image='/images/cream.png'
+                    heading='Curology Product Set'
+                    price='$700'
+                    rating="/images/four-star.png"
+                    category='(145)'
+                />
+            </div>
+
+            <div className='flex flex-wrap justify-center gap-6 mt-6'>
+                <MoreProducts
+                    image='/images/car.png'
+                    heading='Kids Electric Car'
+                    price='$960'
+                    rating="/images/five-star.png"
+                    category='(65)'
+                    dot="/images/dots.png"
+                />
+                <MoreProducts
+                    image='/images/shoes.png'
+                    heading='Jr. Zoom Soccer Cleats'
+                    price='$1160'
+                    rating="/images/five-star.png"
+                    category='(35)'
+                    dot="/images/dots.png"
+                />
+                <MoreProducts
+                    image='/images/child-game.png'
+                    heading='GP11 Shooter USB Gamepad'
+                    price='$660'
+                    rating="/images/five-star.png"
+                    category='(35)'
+                    dot="/images/dots.png"
+                />
+                <MoreProducts
+                    image='/images/jacket.png'
+                    heading='Quilted Satin Jacket'
+                    price='$660'
+                    rating="/images/five-star.png"
+                    category='(55)'
+                    dot="/images/dots.png"
+                />
+            </div>
+            <Features />
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
+
+export default page
